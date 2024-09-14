24fc60de77aa318a64fb622343d2be18d964885d
devAlen01 <alenermekuulu@gmail.com>
2024-09-14 13:10:05 +0600

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.scss";
-import LayoutClient from "./(site)/layout";
 
 const montserratSans = localFont({
   src: "./fonts/Montserrat-VariableFont_wght.ttf",
